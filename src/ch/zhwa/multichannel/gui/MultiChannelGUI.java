b91ce773e550fe606b25c4925d50484e2dc6ffc5
bueti <bueti@offroadfreunde.ch>
2012-03-29 14:26:18 +0200

    Initial add

---
+++ src/ch/zhwa/multichannel/gui/MultiChannelGUI.java
@@ -0,0 +1,141 @@
+package ch.zhwa.multichannel.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class MultiChannelGUI {
+
+	private JFrame frame;
+	private JTextField tFReciever;
+	private JTextField tFSubject;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MultiChannelGUI window = new MultiChannelGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MultiChannelGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 526, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("left:max(12dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.MIN_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("fill:max(70dlu;default):grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.MIN_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblReciever = new JLabel("Empfänger:");
+		frame.getContentPane().add(lblReciever, "2, 2");
+		
+		tFReciever = new JTextField();
+		frame.getContentPane().add(tFReciever, "4, 2, 25, 1, fill, default");
+		tFReciever.setColumns(10);
+		
+		JLabel lblReminder = new JLabel("Reminder:");
+		frame.getContentPane().add(lblReminder, "30, 2");
+		
+		JCheckBox checkBox = new JCheckBox("");
+		frame.getContentPane().add(checkBox, "32, 2, center, default");
+		
+		JLabel lblBetreff = new JLabel("Betreff:");
+		frame.getContentPane().add(lblBetreff, "2, 4");
+		
+		tFSubject = new JTextField();
+		frame.getContentPane().add(tFSubject, "4, 4, 23, 1, fill, default");
+		tFSubject.setColumns(10);
+		
+		JLabel lblTyp = new JLabel("Typ:");
+		frame.getContentPane().add(lblTyp, "28, 4, right, default");
+		
+		JComboBox comboBox = new JComboBox();
+		frame.getContentPane().add(comboBox, "30, 4, 3, 1, fill, default");
+		
+		JLabel lblMessage = new JLabel("Nachricht:");
+		frame.getContentPane().add(lblMessage, "2, 6");
+		
+		JTextArea textArea = new JTextArea();
+		frame.getContentPane().add(textArea, "2, 8, 27, 5, fill, fill");
+		
+		JButton button = new JButton("Abschicken");
+		frame.getContentPane().add(button, "30, 12, 3, 1, fill, fill");
+	}
+
+}
