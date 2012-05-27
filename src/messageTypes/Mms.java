a6ccb2bed249395f2b097d618a0014047a9c911a
Roli <Roli@Ultra-Roli.home>
2012-05-27 12:22:48 +0200

    MMS-Validation hinzugefügt

--- src/messageTypes/Mms.java
+++ src/messageTypes/Mms.java
@@ -1,5 +1,8 @@
 package messageTypes;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Mms extends Message implements IValidator {
 
 	@Override
@@ -10,12 +13,39 @@ public class Mms extends Message implements IValidator {
 	}
 
 	@Override
-	public boolean validate() {
-		// TODO: MMS Validierung:
-		// Mehrere Empfänger?
-		// Format Handling: +417uxxxyyzz, 07uxxxyyyzz, 07u'xxx'yy'zz, +41'7u'xxx'yy'zz, 07u xxx yy zz
+	/** Prüfung ob MMS-Nummer gültig ist oder nicht.
+	 * Falls Nummer ungültig, wird Benutzer Meldung angezeigt.
+	 * Prüfung der MMS-Nummer kontrolliert, ob es sich um eine 
+	 * Schweizer Mobile-Nummer handelt (Falls Landesvorwahl,  
+	 * muss +41 eingetragen sein. Anschliessend muss Ziffer 7 in Vorwahl 
+	 * enthalten sein)
+	 */
+	public boolean validate() throws Exception {
+		if (isValidPhoneNumber(this.getRecipient())) {
+			if (this.getSubject().equals("") && this.getText().equals("")) {
+				throw new Exception("Subject or Text is empty!");
+			}
+		} else {
+			throw new Exception("Telephone-Number for MMS is invalid");
+		}
+
 		return true;
 	}
+		
+	// RegEx für MMS-Number
+	// Format Handling: +417uxxxyyzz, 07uxxxyyyzz, 07u / xxx yy zz,
+	// +41 7u xxx yy zz, 07u xxx yy zz, 07u/xxx yy zz
+		public boolean isValidPhoneNumber(String mmsNumber) {
+			//Vorgängig alle Leerschläge und '/' entfernen in mmsNumber
+			String mmsNumberStripSpaces = mmsNumber.replaceAll("[\\s[/]]","");
+			//String expression = (\+|0)?(41)?[7]{1}\d([/ -]?\d)+;
+			String expression = "(\\+|0)?(41)?[7]{1}\\d([/ -]?\\d)+";
+			CharSequence inputStr = mmsNumberStripSpaces;
+			Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(inputStr);
+			return matcher.matches();
+		}
+
 	
 	public void sendReminder() {
 		System.out.println("\"Das ist der Reminder an die Message: " + this.getSubject() + " an den Empfänger " + this.getRecipient() + "\"");
