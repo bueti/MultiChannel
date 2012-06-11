04f7faf33723f9293a462028ea53c82051fb883a
Roli <Roli@160.85.118.133>
2012-06-11 20:31:58 +0200

    MultiChannelLogMonitor Javadoc und Variabelname scrollbar gesetzt

--- src/gui/MultiChannelLogMonitor.java
+++ src/gui/MultiChannelLogMonitor.java
@@ -9,17 +9,25 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-//TODO Javadoc
+
+
+/**
+ * The MultiChannelLogMonitor class is used to show log-messages include error-messages.
+ *
+ * @author  Benjamin Buetikofer, Roland Hofer
+ * @version 1.0
+ */
+
 public class MultiChannelLogMonitor extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JFrame frame;
 	private JLabel label;
 	private JTextArea area;
-	private JScrollPane bildlauf;
+	private JScrollPane scrollbar;
 	private static MultiChannelLogMonitor _instance = null;
 
-	// Konstruktor
+	
 	
     public static MultiChannelLogMonitor getInstance() {
         if (_instance == null) {
@@ -28,28 +36,35 @@ public class MultiChannelLogMonitor extends JFrame {
         return _instance;
     }
 
+    /**
+	 * Constructor MultiChannelLogMonitor
+	 * starts the frame of MultiChannelLogMonitor
+	 * @param 
+	 */
+    
 	private MultiChannelLogMonitor() {
 		logMonitor();
 	}
-	
-	private void logMonitor() {
+	/**
+	 * Initialises the contents of the frame
+	 * 
+	 */
+		private void logMonitor() {
 		frame = new JFrame("MultiChannel-LogMonitor");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(850, 450);
 		frame.setSize(500, 250);
 		frame.getContentPane().setLayout(new BorderLayout());
 		label = new JLabel("Multi-Channel Log-Einträge");
-		area = new JTextArea();// todo - einfüllen
-		// area einrichten
+		area = new JTextArea();
 		area.setLineWrap(true);
 		area.setWrapStyleWord(true);
 		area.setBackground(Color.WHITE);
 		area.setFont(new Font("SansSerif", Font.PLAIN, 12));
 		area.setEditable(false);
-		// area Bildlaufleisten
-		bildlauf = new JScrollPane();
-		bildlauf.getViewport().setView(area);
-		bildlauf.getViewport().add(area, null);
+		scrollbar = new JScrollPane();
+		scrollbar.getViewport().setView(area);
+		scrollbar.getViewport().add(area, null);
 		frame.getContentPane().add(label, BorderLayout.NORTH);
 		frame.getContentPane().add(area, BorderLayout.CENTER);
 
